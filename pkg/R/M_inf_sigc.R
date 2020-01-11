ecb25d2ea50100cd89a20d2a9820ac9aab188b8a
manuelao <manuelao@83e4a131-4cfb-40c5-8268-7d9afd4fe311>
2020-01-11 13:37:11 +0000

    additions in manual, truncate M_inf & minor changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ra4bayesmeta@3 83e4a131-4cfb-40c5-8268-7d9afd4fe311

--- pkg/R/M_inf_sigc.R
+++ pkg/R/M_inf_sigc.R
@@ -1,14 +1,16 @@
 
-M_inf_sigc <- function(rlmc, df, alpha=0.5){
+M_inf_sigc <- function(rlmc, df, alpha=0.5, truncation=5*10^6){
   # inputs:
   # rlmc: target RLMC value
   # df: data frame in bayesmeta format
   # alpha: quantile of the SGC distribution to use as reference threshold U_ref
+  # truncation: upper bound for the parameter value to aviod numberical problems
   # output:
   # parameter m of the SGC distribution with C=sigma_ref^{-2}
   sigma.ref <- sigma_ref(df)
   log.quot <- -6*log(sigma.ref) + 2*log(1-rlmc) -2*log(rlmc) 
   # return(1- log(alpha)/log(1+exp(log.quot)))
   log.res <- log(-log(1-alpha)) - log(log(1+exp(log.quot)))
-  return(1 + exp(log.res))
+  res <- 1 + exp(log.res)
+  return(min(res, truncation))
 }
